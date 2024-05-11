8f05c11f67941206dde3d07e3d18e9a63ab4a40f
xinkev <lyx13710@gmail.com>
2024-05-11 18:25:00 +0900

    Remove custom colors for now

--- composeApp/src/commonMain/kotlin/feature/theme/Colors.kt
+++ composeApp/src/commonMain/kotlin/feature/theme/Colors.kt
@@ -1,6 +1,5 @@
 package feature.theme
 
-//import androidx.compose.material.lightColors
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
@@ -9,12 +8,7 @@ val brownDarker = Color(0xff9A461F)
 val textColor = Color(0xff333333)
 val bottomBarColor = Color(0xffF1EAEF)
 val dividerColor = Color(0xff79747E)
+val keyboardKeyColor = Color(0xffF1EAEF)
 
 val lightColorScheme = lightColorScheme(
-    primary = brown,
-    primaryContainer = brown,
-    onPrimaryContainer = Color.White,
-    onPrimary = Color(0xfff8f7f7),
-    surface = brown,
-    onSurface = Color(0xfff8f7f7)
 )
