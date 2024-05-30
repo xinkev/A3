ff9b2fd9c0906abbd79ecb1617b75f819434c7ce
xinkev <lyx13710@gmail.com>
2024-05-30 13:46:48 +0900

    enable edge-to-edge in Android

--- composeApp/src/androidMain/kotlin/com/xinkev/a3/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/xinkev/a3/MainActivity.kt
@@ -1,18 +1,19 @@
 package com.xinkev.a3
 
-import presentation.App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.xinkev.a3.di.androidModules
 import di.startKoin
 import org.koin.android.ext.koin.androidContext
+import presentation.App
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             startKoin(
                 appDeclaration = {
