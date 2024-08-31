5f28ee3c1156d2b9d85bb4e90d1fe05c4d2b887d
xinkev <lyx13710@gmail.com>
2024-08-31 16:30:35 +0900

    improve the Settings screen look and also add theme generated by material theme builder

--- composeApp/src/commonMain/kotlin/presentation/feature/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/presentation/feature/settings/SettingsScreen.kt
@@ -7,27 +7,37 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Restore
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import presentation.theme.ListSpecs
-import presentation.theme.brownDarker
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.annotation.KoinExperimentalAPI
 import presentation.composables.CenteredTopBar
+import presentation.theme.Dimen
 
-object SettingsScreen{
+object SettingsScreen {
 
     @OptIn(KoinExperimentalAPI::class)
     @Composable
@@ -46,47 +56,101 @@ object SettingsScreen{
                     .verticalScroll(scrollState)
             ) {
                 AnimatedVisibility(visible = loading) {
-                    LinearProgressIndicator(Modifier.fillMaxWidth(), color = brownDarker)
+                    LinearProgressIndicator(
+                        Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.primary)
                 }
-                Import(onClick = vm::onClickRestore)
-                HorizontalDivider()
+                Group("Backup") {
+                    Import(onClick = vm::onClickRestore)
+                }
             }
         }
     }
 
     @Composable
     private fun Import(onClick: () -> Unit) {
-        LabelOnlySettingsEntry("Import", modifier = Modifier.clickable { onClick() })
-    }
-
-    @Composable
-    private fun LabelOnlySettingsEntry(
-        label: String,
-        modifier: Modifier,
-    ) {
-        SettingsEntry(modifier = modifier, leftContent = {
-            Text(
-                label, style = ListSpecs.textStyle
-            )
-        }, rightContent = {})
+        SettingsEntry(
+            onClick,
+            leftContent = {
+                LabelWithIcon(Icons.Default.Restore, "Restore")
+            }
+        )
     }
 
     @Composable
     private fun SettingsEntry(
-        modifier: Modifier,
-        leftContent: @Composable () -> Unit,
-        rightContent: @Composable () -> Unit
+        onClick: () -> Unit,
+        modifier: Modifier = Modifier,
+        leftContent: @Composable RowScope.() -> Unit,
+        rightContent: @Composable RowScope.() -> Unit = {}
     ) {
-        Box(modifier = Modifier.height(ListSpecs.height).then(modifier)) {
+        Box(
+            modifier = Modifier
+                .height(56.dp)
+                .clickable(onClick = onClick)
+                .then(modifier)
+        ) {
             Row(
-                modifier = Modifier.padding(
-                    horizontal = ListSpecs.horizontalPadding, vertical = ListSpecs.verticalPadding
-                ), horizontalArrangement = Arrangement.spacedBy(ListSpecs.horizontalPadding)
+                modifier = Modifier
+                    .padding(
+                        horizontal = Dimen.largePadding, vertical = Dimen.smallPadding
+                    )
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.spacedBy(Dimen.mediumPadding),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 leftContent()
                 Spacer(Modifier.weight(1f))
                 rightContent()
             }
+        }
+    }
+
+
+    @Composable
+    private fun LabelWithIcon(
+        icon: ImageVector,
+        label: String,
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = Dimen.smallPadding)
+                .fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = label,
+                modifier = Modifier.padding(end = Dimen.largePadding)
+            )
+            Text(
+                label,
+                style = LocalTextStyle.current.merge(
+                    TextStyle(
+                        lineHeightStyle = LineHeightStyle(
+                            alignment = LineHeightStyle.Alignment.Center,
+                            trim = LineHeightStyle.Trim.FirstLineTop,
+                        ),
+                    )
+                ),
+            )
+        }
+    }
+
+    @Composable
+    private fun Group(
+        title: String,
+        modifier: Modifier = Modifier,
+        content: @Composable () -> Unit
+    ) {
+        Column(modifier) {
+            Text(
+                text = title.uppercase(),
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.padding(horizontal = Dimen.largePadding)
+            )
+            Spacer(Modifier.height(Dimen.mediumPadding))
+            content()
         }
     }
 }
---
+++ composeApp/src/commonMain/kotlin/presentation/theme/Type.kt
@@ -0,0 +1,5 @@
+package presentation.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
