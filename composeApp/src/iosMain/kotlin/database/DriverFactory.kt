4dd38b30b6235b3ac08543e8b72f3b22e6d8bfd6
xinkev <lyx13710@gmail.com>
2025-02-14 00:34:34 +0900

    Enable foreign keys in native(iOS) native driver

--- composeApp/src/iosMain/kotlin/database/DriverFactory.kt
+++ composeApp/src/iosMain/kotlin/database/DriverFactory.kt
@@ -2,12 +2,21 @@ package database
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import co.touchlab.sqliter.DatabaseConfiguration
 import com.xinkev.a3.BuildConfig
 import com.xinkev.a3.sqldelight.A3Database
 import core.database.DriverFactory
 
 class IOSDriverFactory : DriverFactory {
     override fun create(): SqlDriver {
-        return NativeSqliteDriver(A3Database.Schema, BuildConfig.dbName)
+        return NativeSqliteDriver(
+            schema = A3Database.Schema,
+            name = BuildConfig.dbName,
+            onConfiguration = { config ->
+                config.copy(
+                    extendedConfig = DatabaseConfiguration.Extended(foreignKeyConstraints = true)
+                )
+            }
+        )
     }
 }
