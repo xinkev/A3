0eb5894c2cd1dd8c3553a079d33e01887c28a1e6
xinkev <lyx13710@gmail.com>
2025-02-14 20:29:23 +0900

    Set a default window size for Desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,6 +1,8 @@
-
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import app.App
 import com.xinkev.a3.BuildConfig
 import di.desktopModules
@@ -10,6 +12,9 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = BuildConfig.appName,
+        state = rememberWindowState(
+            size = DpSize(350.dp, 700.dp)
+        )
     ) {
         startKoin(modules = desktopModules) {
             App()
