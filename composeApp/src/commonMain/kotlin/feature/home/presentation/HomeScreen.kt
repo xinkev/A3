76dca99b6117b2d8494bda1df0358fec515b4b9e
xinkev <lyx13710@gmail.com>
2025-03-30 18:10:05 +0900

    Refactor HomeScreen with extract expense total components
    - Extract daily and monthly total cards into separate composables.
    - Optimize monthly total calculation by using distinctUntilChanged to prevent unnecessary recomputations when only the day changes within the same month.

--- composeApp/src/commonMain/kotlin/feature/home/presentation/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/feature/home/presentation/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -21,6 +22,7 @@ import common.util.toSmartString
 import feature.expense.expense_list.presentation.ExpenseList
 import feature.home.presentation.composables.HomeFab
 import feature.home.presentation.composables.HomeHeader
+import kotlinx.coroutines.flow.StateFlow
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -34,11 +36,7 @@ fun HomeScreen(
 fun HomeScreenContent(
     vm: IHomeViewModel,
 ) {
-    val date by vm.dateMillis.collectAsState()
-    val currentMonthTotal by vm.currentMonthTotal.collectAsState()
-    val currentDateTotal by vm.currentDateTotal.collectAsState()
-    val displayDate = dateTimeToDisplay(date, A3DateFormat.DisplayDate)
-    val currentMonthLabel = dateTimeToDisplay(date, A3DateFormat.MonthFullName)
+    val dateMilli by vm.dateMillis.collectAsState()
 
     Scaffold(
         floatingActionButton = {
@@ -47,7 +45,7 @@ fun HomeScreenContent(
     ) {
         Column {
             HomeHeader(
-                initialDate = date,
+                initialDate = dateMilli,
                 onDateSelected = vm::setDate
             )
             Row(
@@ -55,24 +53,52 @@ fun HomeScreenContent(
                 modifier = Modifier
                     .padding(Dimen.largePadding)
             ) {
-                Card(modifier = Modifier.weight(.5f)) {
-                    Column(modifier = Modifier.padding(Dimen.mediumPadding)) {
-                        Text(displayDate, style = MaterialTheme.typography.titleSmall)
-                        Text(currentDateTotal.toSmartString(), style = MaterialTheme.typography.bodySmall)
-                    }
-                }
-                Card(modifier = Modifier.weight(.5f), onClick = {}) {
-                    Column(modifier = Modifier.padding(Dimen.mediumPadding)) {
-                        Text(currentMonthLabel, style = MaterialTheme.typography.titleSmall)
-                        Text(currentMonthTotal.toSmartString(), style = MaterialTheme.typography.bodySmall)
-                    }
-                }
+                DailyTotal(
+                    dateMilli = dateMilli,
+                    total = vm.currentDateTotal,
+                )
+                MonthlyTotal(
+                    dateMilli = dateMilli,
+                    total = vm.currentMonthTotal,
+                )
             }
             ExpenseList(
                 expenses = vm.expenses,
                 onDeleteConfirmed = vm::onDeleteConfirmed,
                 onClickItem = vm::onClickExpense
             )
+        }
+    }
+}
+
+@Composable
+private fun RowScope.DailyTotal(
+    dateMilli: Long,
+    total: StateFlow<Double>,
+) {
+    val currentDate = dateTimeToDisplay(dateMilli,A3DateFormat.DisplayDate)
+    val totalExpense by total.collectAsState()
+
+    Card(modifier = Modifier.weight(.5f)) {
+        Column(modifier = Modifier.padding(Dimen.mediumPadding)) {
+            Text(currentDate, style = MaterialTheme.typography.titleSmall)
+            Text(totalExpense.toSmartString(), style = MaterialTheme.typography.bodySmall)
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MonthlyTotal(
+    dateMilli: Long,
+    total: StateFlow<Double>,
+) {
+    val month = dateTimeToDisplay(dateMilli, A3DateFormat.MonthFullName)
+    val totalExpense by total.collectAsState()
+
+    Card(modifier = Modifier.weight(.5f), onClick = {}) {
+        Column(modifier = Modifier.padding(Dimen.mediumPadding)) {
+            Text(month, style = MaterialTheme.typography.titleSmall)
+            Text(totalExpense.toSmartString(), style = MaterialTheme.typography.bodySmall)
         }
     }
 }
